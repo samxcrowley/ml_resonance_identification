61e5566b34674fb0ad5fecb0d982807e5a68ab4f
Sam Crowley <samxcrowley@gmail.com>
2026-02-19 12:59:29 +1100

    Add multilayer perceptron for regression

--- model/encoder.py
+++ model/encoder.py
@@ -1,10 +1,11 @@
+import torch
 from torch import nn
 
 from model.layer.encoder_layer import EncoderLayer
 
 class Encoder(nn.Module):
 
-    def __init__(self, d_model, max_len, n_hidden, n_head, n_layers, dropout_p, device):
+    def __init__(self, d_model, max_len, n_hidden, n_head, n_layers, dropout_p):
 
         super().__init__()
 
@@ -14,9 +15,22 @@ class Encoder(nn.Module):
                                                   dropout_p=dropout_p)
                                     for _ in range(n_layers)])
 
+        self.mlp = nn.Sequential(
+            nn.Linear(d_model, d_model * 2),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(d_model * 2, 1)
+        )
+
     def forward(self, x):
 
         for layer in self.layers:
             x = layer(x)
+
+        x = x.mean(dim=1)
+
+        x = self.mlp(x)
+
+        x = torch.sigmoid(x)
 
         return x

bb17681196e05a487662e06a1c70a19330ec5505
Sam Crowley <samxcrowley@gmail.com>
2026-02-25 18:32:34 +1100

    Add multi res targets (mask + regression [energy, total gamma])

--- data.py
+++ data.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import transforms
 from targets import Target
 
+MAX_RESONANCES = 10
+
 # x, y are the axes and z is the value at each point
 x_key = 'theta_cm_out'
 y_key = 'cn_ex'
@@ -15,8 +17,6 @@ z_key = 'dsdO'
 
 # output sequence shape:
 # [n, E, A]
-# output target shape:
-# [n]
 def get_tensors(data_filename, log_cx=True, compressed=True):
 
     data = open_file(f'data/{data_filename}', compressed)
@@ -63,9 +63,7 @@ def get_single_res_targets(data_filename, compressed=True):
     for i in range(n):
 
         points = data[i]['observable_sets'][0]['points']
-
-        xs = sorted(set(p[x_key] for p in points))
-        ys = sorted(set(p[y_key] for p in points))
+        xs, ys = get_xs_ys(points)
 
         n_resonances = len(data[i]['levels'])
 
@@ -87,6 +85,49 @@ def get_single_res_targets(data_filename, compressed=True):
 
     return torch.stack(targets, dim=0)
 
+# returns a mask targets tensor of shape [n, MAX_RESONANCES, 1]
+# and a regression targets tensor of shape [n, MAX_RESONANCES, 2]
+def get_multi_res_targets(data_filename, compressed=True):
+
+    data = open_file(f'data/{data_filename}', compressed)
+
+    n = len(data)
+
+    mask_targets = []
+    reg_targets = []
+    
+    for i in range(n):
+
+        points = data[i]['observable_sets'][0]['points']
+        xs, ys = get_xs_ys(points)
+
+        n_resonances = len(data[i]['levels'])
+
+        mask_target = torch.zeros([MAX_RESONANCES, 1], dtype=torch.float32)
+        reg_target = torch.zeros([MAX_RESONANCES, 2], dtype=torch.float32)
+
+        for n in range(n_resonances):
+
+            level = data[i]['levels'][n]
+
+            mask_target[n, 0] = 1.0
+
+            energy = level['energy']
+            energy = transforms._normalise(energy, min(ys), max(ys))
+            reg_target[n, 0] = energy
+
+            gamma_total = level['Gamma_total']
+            gamma_total = np.log10(gamma_total)
+            reg_target[n, 1] = gamma_total
+
+        mask_targets.append(mask_target)
+        reg_targets.append(reg_target)
+
+    mask_targets = torch.stack(mask_targets, dim=0)
+    reg_targets = torch.stack(reg_targets, dim=0)
+
+    return mask_targets, reg_targets
+
 def open_file(path, compressed=True):
 
     if compressed:
@@ -99,6 +140,13 @@ def open_file(path, compressed=True):
             data = json.load(f)
 
     return data
+
+def get_xs_ys(points):
+
+    xs = sorted(set(p[x_key] for p in points))
+    ys = sorted(set(p[y_key] for p in points))
+
+    return xs, ys
 
 # display a tensor of shape [H, W]
 def display_tensor(tensor, name):

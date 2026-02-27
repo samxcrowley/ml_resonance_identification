b77e2db95cd519b0e6613600f7b7b96f38f1c1c8
Sam Crowley <samxcrowley@gmail.com>
2026-02-27 19:06:31 +1100

    Change output string

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open('params.json', 'r') as f:
 config = params['config']
 
 print('\n--------------------------------\n')
-print(f'Config {config} loaded...')
+print(f'Config `{config}` loaded...')
 print('\n--------------------------------\n')
 
 # train the model and output training results

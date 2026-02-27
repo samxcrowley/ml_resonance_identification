4fb8dcfd4a4f9322058af5af8a0ffa20e377e3e1
Sam Crowley <samxcrowley@gmail.com>
2026-02-28 10:51:05 +1100

    Change positional encoding to sinusoidal

--- model/layer/positional_encoding.py
+++ model/layer/positional_encoding.py
@@ -1,11 +1,23 @@
+import math
 import torch
 from torch import nn
 
 class PositionalEncoding(nn.Module):
 
     def __init__(self, max_len, d_transformer):
+
         super().__init__()
-        self.pos_enc = nn.Parameter(torch.randn(max_len, d_transformer))
+        
+        pe = torch.zeros(max_len, d_transformer)
+        position = torch.arange(0, max_len).unsqueeze(1).float()
+        div = torch.exp(
+            torch.arange(0, d_transformer, 2).float() * (-math.log(10000.0) / d_transformer)
+        )
+
+        pe[:, 0::2] = torch.sin(position * div)
+        pe[:, 1::2] = torch.cos(position * div)
+
+        self.register_buffer('pos_enc', pe)
 
     def forward(self, x):
 
@@ -13,6 +25,6 @@ class PositionalEncoding(nn.Module):
 
         enc = self.pos_enc[:S]
         enc = enc.unsqueeze(0) # [1, S, D]
-        enc = enc.repeat(N, 1, 1) # [N, S, D]
+        enc = enc.expand(N, -1, -1) # [N, S, D]
 
         return enc

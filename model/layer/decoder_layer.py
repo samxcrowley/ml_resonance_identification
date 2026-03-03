Fix normalisation bugs

model/layer/decoder_layer.py
@@ -22,6 +22,7 @@ class DecoderLayer(nn.Module):
 
         # sublayer 1: self-attention
 
+        residual = x
         x = self.norm1(x)
 
         q = (x + query_pos)
@@ -29,10 +30,11 @@ class DecoderLayer(nn.Module):
         v = x
 
         self_att_out, _ = self.self_attention(q, k, v)
-        x = x + self_att_out
+        x = residual + self_att_out
 
         # sublayer 2: cross-attention
 
+        residual = x
         x = self.norm2(x)
 
         q = (x + query_pos)
@@ -40,10 +42,11 @@ class DecoderLayer(nn.Module):
         v = enc
 
         cross_att_out, cross_att = self.cross_attention(q, k, v)
-        x = x + cross_att_out
+        x = residual + cross_att_out
 
         # sublayer 3: feed-forward
 
+        residual = x
         x = self.norm3(x)
 
         ff_out = self.linear1(x)
@@ -51,6 +54,6 @@ class DecoderLayer(nn.Module):
         ff_out = self.dropout(ff_out)
         ff_out = self.linear2(ff_out)
 
-        x = x + ff_out
+        x = residual + ff_out
 
         return x, cross_att

model/layer/encoder_layer.py
@@ -20,6 +20,7 @@ class EncoderLayer(nn.Module):
 
         # sublayer 1: attention
 
+        residual = x
         x = self.norm1(x)
 
         q = (x + pos_enc)
@@ -27,17 +28,18 @@ class EncoderLayer(nn.Module):
         v = x
 
         att_out, _ = self.attention(q, k, v)
-        att_out = x + att_out
+        x = residual + att_out
 
         # sublayer 2: feed-forward
 
-        out = self.norm2(att_out)
+        residual = x
+        out = self.norm2(x)
 
         ff_out = self.linear1(out)
         ff_out = F.relu(ff_out)
         ff_out = self.dropout(ff_out)
         ff_out = self.linear2(ff_out)
 
-        out = att_out + ff_out
+        out = residual + ff_out
 
         return out

model/transformer_decoder.py
@@ -22,6 +22,7 @@ class Transformer_Decoder_Model(nn.Module):
         for layer in self.layers:
 
             x, cross_att = layer(x, enc, pos_enc, query_pos)
-            x = self.norm(x)
+
+        x = self.norm(x)
 
         return x, cross_att